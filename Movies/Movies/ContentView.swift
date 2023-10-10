4622f6916481354c1e26d66f20b69bb02a831921
Frida Bailleres <a01708633@tec.mx>
2023-10-10 11:33:30 -0600

    Crear proyecto y agregar modelo

---
+++ Movies/Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Frida Bailleres González on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies/Movies/Data/Models/Models.swift
@@ -0,0 +1,29 @@
+//
+//  Models.swift
+//  Movies
+//
+//  Created by Frida Bailleres González on 10/10/23.
+//
+
+import Foundation
+
+//{"adult":false,"backdrop_path":"/mRGmNnh6pBAGGp6fMBMwI8iTBUO.jpg","genre_ids":[27,9648,53],"id":968051,"original_language":"en","original_title":"The Nun II","overview":"In 1956 France, a priest is violently murdered, and Sister Irene begins to investigate. She once again comes face-to-face with a powerful evil.","popularity":4160.929,"poster_path":"/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg","release_date":"2023-09-06","title":"The Nun II","video":false,"vote_average":7,"vote_count":776}
+
+struct Movie: Codable {
+  var adult: Bool
+  var backdrop_path: String
+  var genre_ids: String
+  var id: Int
+  var original_language: String
+  var original_title: String
+  var overview: String
+  var popularity: Double
+  var poster_path: String
+  var release_dat: String
+  var title: String
+  var video: Bool
+  var vote_average: Int
+  var vote_count: Int
+}
+  
+
